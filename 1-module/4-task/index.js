4b53762435481afdb7d4b919be776ecef365357c
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-08-02 11:17:10 +0300

    Решение 4 задачи 1 модуля

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,14 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  let regexp1 = /1xbet/;
+  let regexp2 = /xxx/;
+  str = str.toLowerCase();
+
+  if ( str.match(regexp1) || str.match(regexp2) ) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
