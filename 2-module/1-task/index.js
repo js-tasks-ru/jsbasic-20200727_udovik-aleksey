ca55c519fc372a48297d05bc8398a46a52b07cc5
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-08-05 18:59:53 +0300

    Решение 1 задачи 2 модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,31 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
 function sumSalary(salaries) {
   // ваш код...
-}
+
+  let sum = 0;
+
+  for (let key in salaries) {
+
+    if (salaries[key] > 1) {
+      sum += salaries[key];
+    };   
+  };
+
+  if ( sum === 0) {
+    return 0;
+  };
+
+  return sum;
+};
