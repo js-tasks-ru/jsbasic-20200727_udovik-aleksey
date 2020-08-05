84d11aa582f6e4494a99d43694777794c90c7e74
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-08-05 22:02:37 +0300

    Решение 2 задачи 2 модуля

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -3,6 +3,21 @@
  * @param {Object} obj
  * @returns {Boolean}
  */
+
+let schedule = {};
+
 function isEmpty(obj) {
   // ваш код...
+
+  let prop;
+
+  for (let key in obj) {
+    prop = key;
+  }
+
+  if ( prop == undefined ) {
+    return true;
+  }
+
+  return false;
 }
