8b086e7d92f969a03b6c8ba5f3c7344b32753df5
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-08-05 22:57:05 +0300

    Решение 3 задачи 2 модуля

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,21 @@
 let calculator = {
   // ваш код
+
+  num1: 1,
+  num2: 2,
+
+  read(a, b) {
+    this.num1 = a;
+    this.num2 = b;
+  },
+
+  sum() {
+    return +this.num1 + +this.num2;
+  },
+
+  mul() {
+    return +this.num1 * +this.num2;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
