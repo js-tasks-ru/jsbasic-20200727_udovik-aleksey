89845906df916c9ce331c4cbd6f8d7199aaf0f13
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-08-11 11:12:55 +0300

    Решение 5 задачи 3 модуля

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -3,6 +3,28 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+
+const inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
+
 function getMinMax(str) {
   // ваш код...
+  let arr = [];
+  let numArr = [];
+  let result = {
+    min: 0,
+    max: 1,
+  };
+
+  arr = str.split(/[^-^.\d]/g);
+  
+  for (let el of arr) {
+  
+    if (+el !== 0) {
+      numArr.push(+el);
+    }
+  }
+
+  result.min = Math.min(...numArr);
+  result.max = Math.max(...numArr);
+  return result;
 }
