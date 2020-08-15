801cf65e9a6b718964e3a866d81239242ffd3938
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-08-15 16:02:33 +0300

    Решение 2 задачи 4 модуля

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,27 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+
+    for (let i = 0; i < 5; i++) {
+
+        for (let e = 0; e < 5; e++) {
+
+            if (e === i) {
+
+            table.rows[i].cells[e].style.backgroundColor = 'red';
+            }
+        }
+    }
+
+    //  Второй вариант мне кажетс более лаконичным, но npm test он не прошел
+
+    // let td = document.querySelectorAll('td');
+
+    // td.forEach( function (elem, index) {
+
+    //     if (index % 6) {        
+    //     } else {
+    //         elem.style.backgroundColor = 'red';
+    //     }
+    // }); 
 }
