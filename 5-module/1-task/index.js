05cb7523db6adf199b10641825c482e63f800a11
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-08-19 21:20:47 +0300

    Решение 1 задачи 5 модуля

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,11 @@
 function hideSelf() {
   // ваш код...
+
+  let hideButton = document.querySelector('.hide-self-button');
+
+  console.log(hideButton);
+
+  hideButton.addEventListener('click', function(){
+    hideButton.setAttribute('hidden', '');
+  })
 }
