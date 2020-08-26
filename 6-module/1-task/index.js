ecbf98ad5d6e6ff46b9bf61de62e6dde073d8c61
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-08-26 23:30:46 +0300

    Решение 1 задачи 6 модуля

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,50 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = this.createTable(rows);
+    this.elem.addEventListener('click', (event) => this.removeRow(event));
   }
+
+  createTable(rows) {
+    let myTable = document.createElement('table');
+    let myThead = `
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>
+    `;
+    let myTbody = document.createElement('tbody');
+
+    myTable.insertAdjacentHTML('afterbegin', myThead);
+
+    for (let el of rows) {
+      let row = `
+        <tr>
+          <td>${el.name}</td>
+          <td>${el.age}</td>
+          <td>${el.salary}</td>
+          <td>${el.city}</td>
+          <td><button>X</button>
+          </td>
+        </tr>
+      `;
+
+      myTbody.insertAdjacentHTML('beforeend', row);
+    }
+
+    myTable.append(myTbody);
+    return myTable;
+  }
+
+  removeRow(event) { 
+    
+    if (event.target.innerText === 'X') {
+      event.path[2].remove();
+    }
+  } 
 }
