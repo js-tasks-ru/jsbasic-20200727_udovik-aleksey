9d87f651bbe68a9866bd4901a420175555b357e6
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-08-30 12:48:00 +0300

    Решение 3 задачи 6 модуля

--- assets/lib/create-element.js
+++ assets/lib/create-element.js
@@ -4,3 +4,16 @@ export default function(html) {
   div.innerHTML = html;
   return div.firstElementChild;
 };
+
+export function createArrows(carouselParentElement) {
+  let arrows = `
+    <div class="carousel__arrow carousel__arrow_right">
+    <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+  </div>
+  <div class="carousel__arrow carousel__arrow_left">
+    <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+  </div>
+  `;
+  carouselParentElement.insertAdjacentHTML('afterbegin', arrows);
+  return carouselParentElement;
+}
