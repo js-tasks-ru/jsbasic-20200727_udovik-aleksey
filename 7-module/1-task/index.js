e4d483f9c1cd01e0f824eaecc9ff13552f6d713d
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-08-31 06:39:37 +0300

    Решение 1 задачи 7 модуля

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,78 @@
 import createElement from '../../assets/lib/create-element.js';
+import {createMenu} from '../../assets/lib/create-element.js';
 
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+
+    this.elem = createMenu(this.categories);
+    this.makeScrollRibbonMenu(this.elem);
+    this.generateSelectedCategory(this.elem);
   }
+
+  // интерфейс меню
+  makeScrollRibbonMenu(elem) {
+    const ribbonInner = elem.querySelector('.ribbon__inner');
+    const ribbonArrowLeft = elem.querySelector('.ribbon__arrow_left');
+    const ribbonArrowRight = elem.querySelector('.ribbon__arrow_right');
+    
+    // обработчики движения скролла
+    ribbonArrowLeft.addEventListener('click', makeScroll);
+    ribbonArrowRight.addEventListener('click', makeScroll);
+
+    function makeScroll(event) {
+      event.stopPropagation();
+      
+      if (event.currentTarget === ribbonArrowLeft) {
+        ribbonInner.scrollBy(-350, 0);
+      } else if (event.currentTarget === ribbonArrowRight) {
+        ribbonInner.scrollBy(350, 0);
+      }
+    }
+
+    // обработчик скрытия стрелочек
+    ribbonInner.addEventListener('scroll', () => {
+      let scrollWidth = ribbonInner.scrollWidth;
+      let scrollLeft = ribbonInner.scrollLeft;
+      let clientWidth = ribbonInner.clientWidth;
+      let scrollRight = scrollWidth - scrollLeft - clientWidth;
+      
+      if (scrollLeft < 1) {
+        ribbonArrowLeft.classList.remove('ribbon__arrow_visible');
+      } else {
+        ribbonArrowLeft.classList.add('ribbon__arrow_visible');
+      }
+
+      if (scrollRight < 1) {
+        ribbonArrowRight.classList.remove('ribbon__arrow_visible');
+      } else {
+        ribbonArrowRight.classList.add('ribbon__arrow_visible');
+      }
+    });
+  }
+
+  // интерфейс при выборе пункта меню
+  generateSelectedCategory(elem) {
+    
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      // создаю массив ссылок <a> из элемента <nav class="ribbon__inner">
+      let elemArray = elem.querySelectorAll('.ribbon__item');
+
+      // прохожусь по массиву и удаляю класс 'ribbon__item_active', если такой имеется
+      elemArray.forEach(element => {
+        element.classList.remove('ribbon__item_active');
+      });
+      // добавляю класс 'ribbon__item_active' у выбранного элемента
+      event.target.classList.add('ribbon__item_active');
+
+      let ribbonSelected = new CustomEvent('ribbon-select', {
+        detail: event.target.closest('.ribbon__item').dataset.id,
+        bubbles: true,
+      });
+      elem.dispatchEvent(ribbonSelected);
+    });
+  }
+  
 }
--- assets/lib/create-element.js
+++ assets/lib/create-element.js
@@ -5,6 +5,7 @@ export default function(html) {
   return div.firstElementChild;
 };
 
+// функция для создания RabbonMenu
 export function createArrows(carouselParentElement) {
   let arrows = `
     <div class="carousel__arrow carousel__arrow_right">
@@ -16,4 +17,53 @@ export function createArrows(carouselParentElement) {
   `;
   carouselParentElement.insertAdjacentHTML('afterbegin', arrows);
   return carouselParentElement;
+}
+
+
+export function createMenu(categories) {
+
+  const ribbonElement = document.createElement('div');
+  ribbonElement.classList.add('ribbon');
+
+  // левая стрелочка
+  const createRibbonArrowLeft = function(ribbonElement) {
+    const ribbonArrowLeft = `
+      <button class="ribbon__arrow ribbon__arrow_left">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+    `;
+    ribbonElement.insertAdjacentHTML('afterbegin', ribbonArrowLeft);
+    return ribbonElement;
+  }
+
+  createRibbonArrowLeft(ribbonElement);
+
+  // список меню
+  const ribbonInner = document.createElement('nav');
+  ribbonInner.classList.add('ribbon__inner');
+
+  categories.forEach(category => {
+    let ribbonItem = `
+      <a href="#" class="ribbon__item" data-id="${category.id}">${category.name}</a>
+    `;
+    ribbonInner.insertAdjacentHTML('beforeend', ribbonItem);
+  });
+
+  ribbonElement.insertAdjacentElement('beforeend', ribbonInner);
+
+
+  // правая стрелочка
+  const createRibbonArrowRight = function(ribbonElement) {
+    const ribbonArrowRight = `
+      <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+    `;
+    ribbonElement.insertAdjacentHTML('beforeend', ribbonArrowRight);
+    return ribbonElement;
+  }
+
+  createRibbonArrowRight(ribbonElement);
+
+  return ribbonElement;
 }
